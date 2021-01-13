d26978ac8f076e6616f16faba218ffe2f0306160
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-01-13 09:24:49 +0200

    import multi boards lib

---
+++ libs/stm32-discovery-DISCO_L475VG_IOT/config.ts
@@ -0,0 +1,43 @@
+namespace config {
+    export const PIN_SPEAKER_AMP = DAL.PA_5;
+    export const PIN_A0 = DAL.PC_5;
+    export const PIN_A1 = DAL.PC_4;
+    export const PIN_A2 = DAL.PC_3;
+    export const PIN_A3 = DAL.PC_2;
+    export const PIN_A4 = DAL.PC_1;
+    export const PIN_A5 = DAL.PC_0;
+
+    export const PIN_D0 = DAL.PA_1;
+    export const PIN_D1 = DAL.PA_0;
+    export const PIN_D2 = DAL.PD_14;
+    export const PIN_D3 = DAL.PB_0;
+    export const PIN_D4 = DAL.PA_3;
+    export const PIN_D5 = DAL.PB_4;
+    export const PIN_D6 = DAL.PB_1;
+    export const PIN_D7 = DAL.PA_4;
+    export const PIN_D8 = DAL.PB_2;
+    export const PIN_D9 = DAL.PA_15;
+    export const PIN_D10 = DAL.PA_2;
+    export const PIN_D11 = DAL.PA_7;
+    export const PIN_D12 = DAL.PA_6;
+    export const PIN_D13 = DAL.PA_5;
+    export const PIN_D14 = DAL.PB_9;
+    export const PIN_D15 = DAL.PB_8;
+    
+    export const PIN_RX = DAL.PA_1;
+    export const PIN_TX = DAL.PA_0;
+    export const PIN_MISO = DAL.PA_6;
+    export const PIN_MOSI = DAL.PA_7;
+    export const PIN_SCK = DAL.PA_5;
+    export const PIN_SCL = DAL.PB_8;
+    export const PIN_SDA = DAL.PB_9;
+    
+    export const PIN_LED = DAL.PA_5;
+    export const PIN_LED2 = DAL.PB_14;
+    export const PIN_LED3 = DAL.PC_9;
+    export const PIN_LED4 = DAL.PC_9;
+    
+    export const PIN_ACCELEROMETER_INT = DAL.PD_11; 
+    export const PIN_ACCELEROMETER_SCL = DAL.PB_10;
+    export const PIN_ACCELEROMETER_SDA = DAL.PB_11; 
+}
