5ae7aaa3ead99335e339eeea49b6ce5f38bff69d
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-02-23 17:29:01 +0100

    Magic reference to get standard typescript libs

--- sim/dalboard.ts
+++ sim/dalboard.ts
@@ -1,3 +1,4 @@
+/// <reference path="../node_modules/typescript/lib/lib.d.ts"/>
 /// <reference path="../node_modules/pxt-core/built/pxtsim.d.ts"/>
 /// <reference path="../node_modules/pxt-core/localtypings/pxtarget.d.ts"/>
 /// <reference path="../built/common-sim.d.ts"/>
