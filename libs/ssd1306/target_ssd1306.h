9d37e7fb227d9159dfcd193c3c675ae129b86394
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-08-31 14:13:50 +0000

    Remove model dependency

--- libs/ssd1306/target_ssd1306.h
+++ libs/ssd1306/target_ssd1306.h
@@ -1,17 +1,20 @@
-#include "STM32DISCO_L475VG_IOT.h"
 #include "pxt.h"
 #include "ssd1306.h"
 
 namespace pxt {
 class WSSD1306 {
+  private:
+    CODAL_I2C i2c;
+
   public:
     codal::SSD1306_I2C screen;
-    STM32DISCO_L475VG_IOT iotNode;
 
     static constexpr uint16_t I2C_ADDRESS = 0x78;
     static constexpr uint16_t WIDTH = 128;
     static constexpr uint16_t HEIGHT = 64;
 
-    WSSD1306() : screen(iotNode.i2c1, I2C_ADDRESS, WIDTH, HEIGHT) { screen.init(); }
+    WSSD1306() : i2c(*LOOKUP_PIN(SDA), *LOOKUP_PIN(SCL)), screen(i2c, I2C_ADDRESS, WIDTH, HEIGHT) {
+        screen.init();
+    }
 };
 } // namespace pxt
