84d92bba4d82f900814c0c0a0737b033e33f5d20
Sébastien NEDJAR <sebastien@nedjar.com>
2022-01-13 13:19:19 +0000

    Change visibility

--- libs/stm32-discovery-DISCO_L475VG_IOT/ns.ts
+++ libs/stm32-discovery-DISCO_L475VG_IOT/ns.ts
@@ -21,8 +21,3 @@ namespace loops {
 namespace serial {
 
 }
-
-//% color=#00c0c1 weight=5 icon="\uf07b" avanced=true
-namespace storage {
-
-}
---
+++ libs/storage/ns.ts
@@ -0,0 +1,4 @@
+//% color=#00c0c1 weight=5 icon="\uf07b" avanced=true
+namespace storage {
+
+}
