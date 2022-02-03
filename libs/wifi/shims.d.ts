c0419b5585565cd7ba3bfe6c9ae30c16852b6c5e
Sébastien NEDJAR <sebastien@nedjar.com>
2022-02-03 20:57:03 +0000

    Add connection fonctions

--- libs/wifi/shims.d.ts
+++ libs/wifi/shims.d.ts
@@ -11,6 +11,21 @@ declare namespace wifi {
      */
     //% shim=wifi::numberOfNetworksVisible
     function numberOfNetworksVisible(): int32;
+
+    /**
+     */
+    //% shim=wifi::connectToANetwork
+    function connectToANetwork(ssid: string, passphrase: string): void;
+
+    /**
+     */
+    //% shim=wifi::connected
+    function connected(): boolean;
+
+    /**
+     */
+    //% shim=wifi::disconnectFromANetwork
+    function disconnectFromANetwork(): void;
 }
 
 // Auto-generated. Do not edit. Really.
