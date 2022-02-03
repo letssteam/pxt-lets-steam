7a7892c8c0351fc1767a88226d045e961dd75ca5
Sébastien NEDJAR <sebastien@nedjar.com>
2022-02-03 14:14:28 +0000

    Add target mecanism

---
+++ libs/wifi/target_wifi.h
@@ -0,0 +1,17 @@
+#include "pxt.h"
+#include "ISM43362_M3G_L44_driver.h"
+
+namespace pxt {
+class WWifi {
+  private:
+    CODAL_SPI *spi;
+
+  public:
+    IsmDrvClass wifi;
+
+    WWifi()
+        : spi(pxt::getSPI(LOOKUP_PIN(WIFI_ISM43362_MOSI), LOOKUP_PIN(WIFI_ISM43362_MISO), LOOKUP_PIN(WIFI_ISM43362_SCK))),
+          wifi(spi, LOOKUP_PIN(WIFI_ISM43362_CS), LOOKUP_PIN(WIFI_ISM43362_COMMAND_DATA_READY), LOOKUP_PIN(WIFI_ISM43362_RESET),
+               LOOKUP_PIN(WIFI_ISM43362_WAKE_UP)) {}
+};
+} // namespace pxt
