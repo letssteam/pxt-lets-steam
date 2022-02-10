60ba26ee82cf6c69446d92c362e76b8ccbd6c961
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-02-10 08:49:09 +0000

    fix I2C screen (again)

--- libs/oled/ssd1306.cpp
+++ libs/oled/ssd1306.cpp
@@ -27,20 +27,11 @@ constexpr uint16_t toLocalColor(PixelColor color) {
 
 uint16_t parseStrHex(std::string str) {
 
-    try {
-        if (str.size() > 4) {
-            return std::stoul(str.substr(0, 4), nullptr, 16)
-        }
-
-        return std::stoul(str, nullptr, 16);
-    } catch (...) {
-        return 0;
+    if (str.size() > 4) {
+        return std::stoul(str.substr(0, 4), nullptr, 16);
     }
-}
 
-void defaultInit() {
-    getWSSD1306()->screen = codal::SSD1306_I2C(i2c, I2C_ADDRESS, WIDTH, HEIGHT, false);
-    getWSSD1306()->isInitialized = true;
+    return std::stoul(str, nullptr, 16);
 }
 
 /**
@@ -50,10 +41,9 @@ void defaultInit() {
 //% group="More" advanced=true
 //% help=screen/ssd1306_init_screen
 //% blockId=ssd1306_init_screen block="Initialized screen on address 0x%address, with width %width and height %height"
-//% parts="ssd1306" weight=95
-void initScreen(String address = 78, uint16_t width = 128, uint16_t height = 64) {
-    getWSSD1306()->screen = codal::SSD1306_I2C(i2c, parseStrHex(str->getUTF8Data()), width, height, false);
-    getWSSD1306()->isInitialized = true;
+//% parts="ssd1306" weight=95 address.defl=78 width.defl=128 height.defl=64
+void initScreen(String address, uint16_t width, uint16_t height) {
+    getWSSD1306()->initScreen(parseStrHex(address->getUTF8Data()), width, height);
 }
 
 /**
@@ -69,9 +59,6 @@ void initScreen(String address = 78, uint16_t width = 128, uint16_t height = 64)
 //% blockId=print_string_ssd1306 block="print %str on screen in %color at x: %x|y: %y"
 //% parts="ssd1306"
 void printString(String str, PixelColor color, uint8_t x, uint8_t y) {
-    if (!getWSSD1306()->isInitialized)
-        defaultInit();
-
     getWSSD1306()->screen.drawText(str->getUTF8Data(), x, y, toLocalColor(color));
     getWSSD1306()->screen.show();
 }
@@ -86,9 +73,6 @@ void printString(String str, PixelColor color, uint8_t x, uint8_t y) {
 //% blockId=fill_screen_ssd1306 block="fill the screen in %color"
 //% parts="ssd1306"
 void fillScreen(PixelColor color) {
-    if (!getWSSD1306()->isInitialized)
-        defaultInit();
-
     getWSSD1306()->screen.fill(toLocalColor(color));
     getWSSD1306()->screen.show();
 }
@@ -105,9 +89,6 @@ void fillScreen(PixelColor color) {
 //% blockId=set_pixel_ssd1306 block="set pixel at %x, %y to %color"
 //% parts="ssd1306"
 void setPixel(uint8_t x, uint8_t y, PixelColor color) {
-    if (!getWSSD1306()->isInitialized)
-        defaultInit();
-
     getWSSD1306()->screen.drawPixel(x, y, toLocalColor(color));
     getWSSD1306()->screen.show();
 }
@@ -122,9 +103,6 @@ void setPixel(uint8_t x, uint8_t y, PixelColor color) {
 //% blockId=invert_screen_ssd1306 block="invert the screen : %invert"
 //% parts="ssd1306"
 void invertScreen(bool invert) {
-    if (!getWSSD1306()->isInitialized)
-        defaultInit();
-
     getWSSD1306()->screen.invert(invert);
     getWSSD1306()->screen.show();
 }
--- libs/oled/target_ssd1306.h
+++ libs/oled/target_ssd1306.h
@@ -7,17 +7,19 @@ class WSSD1306 {
     CODAL_I2C *i2c;
 
   public:
-    codal::SSD1306_I2C screen;
-    bool isInitialized;
-
     static constexpr uint16_t I2C_ADDRESS = 0x78;
     static constexpr uint16_t WIDTH = 128;
     static constexpr uint16_t HEIGHT = 64;
 
-    WSSD1306()
-        : i2c(pxt::getI2C(LOOKUP_PIN(SDA), LOOKUP_PIN(SCL))), isInitialized(false)
-    /*screen(i2c, I2C_ADDRESS, WIDTH, HEIGHT, false)*/ {
-        // screen.init();
+    codal::SSD1306_I2C screen;
+
+    WSSD1306() : i2c(pxt::getI2C(LOOKUP_PIN(SDA), LOOKUP_PIN(SCL))), screen(i2c, I2C_ADDRESS, WIDTH, HEIGHT, false) {
+        screen.init();
+    }
+
+    void initScreen(uint16_t address, uint16_t width, uint16_t height) {
+        screen = codal::SSD1306_I2C(i2c, address, width, height, false);
+        screen.init();
     }
 };
 } // namespace pxt
