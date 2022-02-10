3019e90364f02fc0acc2cd87fbc6a9f3dc6a4623
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-02-10 11:04:38 +0000

    Improve Oled I2C address selection

--- libs/oled/enums.d.ts
+++ libs/oled/enums.d.ts
@@ -8,4 +8,12 @@
     White = 1,
     }
 
+
+    declare const enum AddressScreen {
+    //% block="0x78"
+    x74 = 0x78,
+    //% block="0x7A"
+    x7A = 0x7A,
+    }
+
 // Auto-generated. Do not edit. Really.
