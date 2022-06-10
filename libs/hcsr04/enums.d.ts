4b2bf1771db570f033c022c202625d46cda901af
Adrien Virieux <adrien.virieux@etu.univ-amu.fr>
2022-06-10 17:12:12 +0200

    adding enums.d file

---
+++ libs/hcsr04/enums.d.ts
@@ -0,0 +1,23 @@
+// Auto-generated. Do not edit.
+
+
+    declare const enum MHCSR04Unit {
+    //% block="m"
+    M = 0,
+    //% block="dm"
+    Dm = 1,
+    //% block="cm"
+    Cm = 2,
+    //% block="mm"
+    Mm = 3,
+    }
+
+
+    declare const enum MDistanceBehold {
+    //% block="Near"
+    Near = 0,
+    //% block="Far"
+    Far = 1,
+    }
+
+// Auto-generated. Do not edit. Really.
