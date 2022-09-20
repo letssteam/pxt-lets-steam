401b84c912bc0da8e1ecf22aa798abaef3b8d9c1
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-09-20 08:46:20 +0000

    Fix #112 Force the magnetormeter configuration

--- libs/accelerometer---lsm6dsl/target_compass.h
+++ libs/accelerometer---lsm6dsl/target_compass.h
@@ -9,15 +9,18 @@ namespace pxt {
 class WCompas {
   private:
     CODAL_I2C *i2c;
-    codal::CoordinateSpace coordinateSpace;
+    codal::CoordinateSpace *coordinateSpace;
 
   public:
     codal::Compass *magnetometer;
 
     WCompas()
         : i2c(getI2C(LOOKUP_PIN(LSM6DSL_SDA), LOOKUP_PIN(LSM6DSL_SCL))),
-          coordinateSpace(SIMPLE_CARTESIAN, true, COORDINATE_SPACE_ROTATED_180),
-          magnetometer(new codal::LIS3MDL_Magnetometer(i2c, 0x3C, coordinateSpace)) {}
+          coordinateSpace(
+              new codal::CoordinateSpace(codal::CoordinateSystem::SIMPLE_CARTESIAN)), //, true, COORDINATE_SPACE_ROTATED_180),
+          magnetometer(new codal::LIS3MDL_Magnetometer(i2c, 0x3C, *coordinateSpace)) {
+        magnetometer->configure();
+    }
 
     ~WCompas() { delete magnetometer; }
 };
