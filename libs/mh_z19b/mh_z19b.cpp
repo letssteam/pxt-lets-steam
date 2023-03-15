05de16427df3bd9a9ce49474acc31be51d98bd0f
Jonathan BAUDIN <jjbaudin@gmail.com>
2023-03-15 15:45:50 +0000

    Implement driver

--- libs/mh_z19b/mh_z19b.cpp
+++ libs/mh_z19b/mh_z19b.cpp
@@ -1,6 +1,35 @@
 #include "pxt.h"
+#include "PulseIn.h"
+#include <vector>
+
+struct PulseInPinObject {
+    DigitalInOutPin pin;
+    codal::PulseIn *pulseIn;
+};
+
+constexpr size_t MAX_SIZE_PULSEIN_MAP = 32;
+
+std::vector<PulseInPinObject> pulseInMap;
 
 namespace input {
+
+codal::PulseIn *getPulseInForPin(DigitalInOutPin pin) {
+    for (int i = 0; i < pulseInMap.size(); ++i) {
+        auto obj = pulseInMap[i];
+        if (obj.pin == pin) {
+            return obj.pulseIn;
+        }
+    }
+
+    if (pulseInMap.size() >= MAX_SIZE_PULSEIN_MAP) {
+        return nullptr;
+    }
+
+    codal::PulseIn *pi = new codal::PulseIn(*pin);
+    pulseInMap.push_back(PulseInPinObject{.pin = pin, .pulseIn = pi});
+
+    return pi;
+}
 
 /**
  * @brief Get the CO2 concentration
@@ -17,7 +46,20 @@ namespace input {
 //% pin.fieldOptions.columns=4
 //% trackArgs=0
 int getCO2Concentration(DigitalInOutPin pin) {
-    return -101; // pin->getAnalogValue();
+
+    auto pi = getPulseInForPin(pin);
+
+    if (pi == nullptr) {
+        return -10;
+    }
+
+    pin->setPolarity(1);
+    int pulse = pi->awaitPulse(2000000) / 1000;
+
+    if (pulse < 0) {
+        return -20;
+    }
+    return 5000 * (pulse - 2) / 1000;
 }
 
 } // namespace input
