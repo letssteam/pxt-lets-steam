8a1b3728cb1f0f621ce052f5d9acd307d2baaf09
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2023-04-28 08:12:37 +0000

    blocks added

---
+++ libs/anemometer/anemometer.cpp
@@ -0,0 +1,62 @@
+#include "pxt.h"
+
+enum class AnemometerUnit {
+    //% block="RPM"
+    RPM = 0,
+    //% block="RPS"
+    RPS = 1
+};
+
+namespace input {
+
+/**
+ * @brief Get the anemometer cup rotation frequency
+ *
+ * @param pin the pin where the anemometer is connected
+ * @param timeSpan the time length of the measurement in seconds
+ * @param unit the measurement unit
+ * @return int
+ */
+//% blockId=input_anemometer_get_rotation_per_unit block="get anemometer rotation on %pin for %timeSpan seconds in %unit"
+//% help=input/anemometer_get_rotation_per_unit
+//% parts="anemometer"
+//% group="Anemometer" weight="90"
+//% pin.fieldEditor="gridpicker"
+//% pin.fieldOptions.width=220
+//% pin.fieldOptions.columns=4
+//% trackArgs=0
+int getAnemometerRotationPerUnit(DigitalInOutPin pin, int timeSpan, AnemometerUnit unit) {
+    switch (unit) {
+    case AnemometerUnit::RPS:
+        return getAnemometerRotation(pin, timeSpan) / timeSpan;
+    case AnemometerUnit::RPM:
+        return getAnemometerRotation(pin, timeSpan) / timeSpan * 60;
+    }
+}
+
+/**
+ * @brief Get the anemometer cup rotation frequency
+ *
+ * @param pin the pin where the anemometer is connected
+ * @param timeSpan the time length of the measurement in seconds
+ * @return int
+ */
+//% blockId=input_anemometer_get_rotation block="get anemometer number of rotations on %pin for %timeSpan seconds"
+//% help=input/anemometer_get_rotation
+//% parts="anemometer"
+//% group="Anemometer" weight="90"
+//% pin.fieldEditor="gridpicker"
+//% pin.fieldOptions.width=220
+//% pin.fieldOptions.columns=4
+//% trackArgs=0
+int getAnemometerRotation(DigitalInOutPin pin, int timeSpan) {
+    int endMeasureTime = current_time_ms() + (timeSpan * 1000);
+    int rotations = 0;
+    while (current_time_ms() < endMeasureTime) {
+        if (pin.getDigitalValue() == 1)
+            ++rotations;
+    }
+    return rotations;
+}
+
+} // namespace input
---
+++ libs/anemometer/shims.d.ts
@@ -0,0 +1,40 @@
+// Auto-generated. Do not edit.
+declare namespace input {
+
+    /**
+     * @brief Get the anemometer cup rotation frequency
+     *
+     * @param pin the pin where the anemometer is connected
+     * @param timeSpan the time length of the measurement in seconds
+     * @param unit the measurement unit
+     * @return int
+     */
+    //% blockId=input_anemometer_get_rotation_per_unit block="get anemometer rotation on %pin for %timeSpan seconds in %unit"
+    //% help=input/anemometer_get_rotation_per_unit
+    //% parts="anemometer"
+    //% group="Anemometer" weight="90"
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.width=220
+    //% pin.fieldOptions.columns=4
+    //% trackArgs=0 shim=input::getAnemometerRotationPerUnit
+    function getAnemometerRotationPerUnit(pin: DigitalInOutPin, timeSpan: int32, unit: AnemometerUnit): int32;
+
+    /**
+     * @brief Get the anemometer cup rotation frequency
+     *
+     * @param pin the pin where the anemometer is connected
+     * @param timeSpan the time length of the measurement in seconds
+     * @return int
+     */
+    //% blockId=input_anemometer_get_rotation block="get anemometer number of rotations on %pin for %timeSpan seconds"
+    //% help=input/anemometer_get_rotation
+    //% parts="anemometer"
+    //% group="Anemometer" weight="90"
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.width=220
+    //% pin.fieldOptions.columns=4
+    //% trackArgs=0 shim=input::getAnemometerRotation
+    function getAnemometerRotation(pin: DigitalInOutPin, timeSpan: int32): int32;
+}
+
+// Auto-generated. Do not edit. Really.
