877a6ac5399e5e72846a4d4073bedbdac607c5b8
Jonathan BAUDIN <jjbaudin@gmail.com>
2023-04-28 13:49:27 +0000

    Implement S/cm driver

--- libs/ecmeter_sen0244/ecmeter_sen0244.cpp
+++ libs/ecmeter_sen0244/ecmeter_sen0244.cpp
@@ -1,17 +1,28 @@
 #include "pxt.h"
 
 constexpr uint8_t EC_SAMPLE = 30;
-constexpr float EC_VREF = 3.3;
+constexpr float EC_VREF = 3.3f;
+constexpr float EC_TO_VOLT = EC_VREF / 1024.0f;
+constexpr float EC_K_FACTOR = 39.78885f;
+
+enum class ECUnit {
+    //% block="S/cm"
+    S_CM,
+    //% block="mS/cm"
+    MS_CM,
+    //% block="µS/cm"
+    US_CM
+};
 
 namespace input {
 
 /**
- * @brief Get the CO2 concentration
+ * @brief Get the EC
  *
  * @param pin the pin where the sensor is connected
  * @return int
  */
-//% blockId=input_get_ec_value block="read ElectroConductivity in %unit on %pin"
+//% blockId=input_get_ec_value block="read ElectroConductivity on %pin in %unit"
 //% help=input/input_get_ec_value
 //% parts="ecmeter_sen0244"
 //% group="Electroconductivity" weight="90"
@@ -19,18 +30,30 @@ namespace input {
 //% pin.fieldOptions.width=220
 //% pin.fieldOptions.columns=4
 //% trackArgs=0
-int getECValue(ECUnit unit, DigitalInOutPin pin) {
-    float value = 0f;
+float getECValue(AnalogInPin pin, ECUnit unit) {
+    float voltage = 0f;
 
     for (uint8_t i = 0; i < EC_SAMPLE; ++i) {
-        value += pin->getAnalogValue();
+        voltage += pin->getAnalogValue();
     }
 
-    value /= (float)EC_SAMPLE;
-    value *= EC_VREF / 1024.0;
+    voltage /= (float)EC_SAMPLE;
+    voltage *= EC_TO_VOLT;
 
-    float ppm = (133.42 * value * vaue * value - 255.86 * value * value + 857.39 * value) * 0.5;
-    return ppm;
+    float resistance = (5600.0 / voltage) * 0.204;
+    float conductivity = (1.0f / resistance) * EC_K_FACTOR;
+
+    switch (unit) {
+    case US_CM:
+        return conductivity * 1000000.0f;
+
+    case MS_CM:
+        return conductivity * 1000.0f;
+
+    case S_CM:
+    default:
+        return conductivity;
+    }
 }
 
 } // namespace input
