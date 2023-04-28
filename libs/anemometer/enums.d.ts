07ae7cafea3ec792840eccc114907f479a2fe6a7
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2023-04-28 08:12:37 +0000

    blocks added

---
+++ libs/anemometer/enums.d.ts
@@ -0,0 +1,11 @@
+// Auto-generated. Do not edit.
+
+
+    declare const enum AnemometerUnit {
+    //% block="RPM"
+    RPM = 0,
+    //% block="RPS"
+    RPS = 1,
+    }
+
+// Auto-generated. Do not edit. Really.
